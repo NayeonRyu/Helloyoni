c33b7bba8b5f4e9d827efdbaa263a4f71c7e366a
NayeonRyu <helloyoni1771@gmail.com>
2023-08-20 15:44:57 +0900

    update helloyoni

--- helloyoni.php
+++ helloyoni.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: helloyoni
-Plugin URI: https://www.notion.so/202307-5471fe8c5b3c4000a62adb2453a6a872
+Plugin URI: https://github.com/NayeonRyu/Helloyoni.git
 Description: helloyoni
 Version: 0.0.1
 Author: yoni
-Author URI: https://www.notion.so/202307-5471fe8c5b3c4000a62adb2453a6a872
+Author URI: https://github.com/NayeonRyu/Helloyoni.git
 License: A "Slug" license name e.g. GPL2
 */
 ?>
