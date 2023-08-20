36e1db2c4fdc4e553671944cf56478a06201aa23
NayeonRyu <helloyoni1771@gmail.com>
2023-08-20 15:49:17 +0900

    update helloyoni.php

--- helloyoni.php
+++ helloyoni.php
@@ -4,8 +4,10 @@ Plugin Name: helloyoni
 Plugin URI: https://github.com/NayeonRyu/Helloyoni.git
 Description: helloyoni
 Version: 0.0.1
-Author: yoni
+Author: Yoni
 Author URI: https://github.com/NayeonRyu/Helloyoni.git
 License: A "Slug" license name e.g. GPL2
 */
+
+
 ?>
